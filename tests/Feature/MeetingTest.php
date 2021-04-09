891c30c27b0a333abdaac445a717fdead7535e1e
Robinson Moraga <robinson.moraga@doctum.net>
2021-04-09 09:08:35 -0400

    fix test

--- tests/Feature/MeetingTest.php
+++ tests/Feature/MeetingTest.php
@@ -3,7 +3,12 @@
 namespace Tests\Feature;
 
 use App\Models\Meeting;
+use App\Models\User;
+use App\Notifications\MeetingNotification;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class MeetingTest extends TestCase
@@ -12,6 +17,10 @@ class MeetingTest extends TestCase
 
     function test_list_meetings()
     {
+        $this->withoutExceptionHandling();
+
+        $this->signIn();
+
         $meeting = Meeting::factory()->create();
 
         $this->get('api/v1/meetings')
@@ -25,34 +34,92 @@ class MeetingTest extends TestCase
             ]);
     }
 
-    // show
     function test_show_a_meeting()
     {
-        $this->withoutExceptionHandling();
-        $meeting = Meeting::factory()->create(['id' => $this->id]);
+        $this->signIn();
 
-        $this->get('api/v1/meetings'.$meeting->id)
+        $meeting = Meeting::factory()->create();
+
+        $this->getJson('api/v1/meetings/'.$meeting->id)
             ->assertStatus(200)
             ->assertJson([
                 'data'  => [
-                    0   => [
-                        'id'    => $meeting->id,
-                    ],
+                    'id'    => $meeting->id,
                 ]
             ]);
     }
 
-
     function test_create_a_meeting()
     {
+        $this->withoutExceptionHandling();
+
+        $this->signIn();
+
+        Storage::fake();
+        Notification::fake();
+
         $data = Meeting::factory()->make();
+
+        $participant = User::factory()->create();
+        $participant2 = User::factory()->create();
 
         $parameters = [
             'name'          => $data->name,
             'description'   => $data->description,
+            'date'          => $data->date,
+            'participants'  => [$participant->id, $participant2->id],
+            'file'          => UploadedFile::fake()->create('test.pdf'),
         ];
 
         $this->post('api/v1/meetings', $parameters)
+            ->assertStatus(201)
+            ->assertJson([
+                'data'  => [
+                    'name'  => $data->name,
+                ]
+            ]);
+
+        $this->assertDatabaseHas('meetings', [
+            'name'          => $data->name,
+            'description'   => $data->description,
+            'date'          => $data->date,
+        ]);
+
+        $this->assertDatabaseHas('meeting_participants', [
+            'meeting_id'        => Meeting::max('id'),
+            'participant_id'    => $participant->id,
+        ]);
+
+        $this->assertDatabaseHas('meeting_participants', [
+            'meeting_id'        => Meeting::max('id'),
+            'participant_id'    => $participant2->id,
+        ]);
+
+        $this->assertDatabaseHas('files', [
+            'fileable_id'       => Meeting::max('id'), // 1
+            'fileable_type'     => Meeting::class, // App\\Models\\Meeting
+            'name'              => 'test.pdf'
+        ]);
+
+        Notification::assertSentTo($participant, MeetingNotification::class);
+    }
+
+    function test_update_a_meeting()
+    {
+        $this->signIn();
+
+        $this->withoutExceptionHandling();
+
+        $meeting = Meeting::factory()->create();
+
+        $data = Meeting::factory()->make();
+        $parameters = [
+            'name'          => $data->name,
+            'description'   => $data->description,
+            'date'          => $data->date,
+        ];
+
+        $this->put('api/v1/meetings/'.$meeting->id, $parameters)
             ->assertStatus(200)
             ->assertJson([
                 'data'  => [
@@ -63,17 +130,18 @@ class MeetingTest extends TestCase
         $this->assertDatabaseHas('meetings', [
             'name'          => $data->name,
             'description'   => $data->description,
+            'date'          => $data->date,
         ]);
+
     }
 
-    // update
-
-    // destroy
     function test_destroy_a_meeting()
     {
-        $this->withoutExceptionHandling();
+        $this->signIn();
+
         $meeting = Meeting::factory()->create();
-        $this->delete('api/v1/meetings'.$meeting->id)
+
+        $this->delete('api/v1/meetings/'.$meeting->id)
             ->assertStatus(204);
 
         $this->assertSoftDeleted('meetings',[
